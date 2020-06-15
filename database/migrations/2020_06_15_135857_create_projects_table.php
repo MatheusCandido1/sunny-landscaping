59424889638cea07e9f869477fdc82a2e70d97a8
Matheus Carvalho <matheus9bh@hotmail.com>
2020-06-15 11:04:14 -0300

    add-project-scaffolding

--- database/migrations/2020_06_15_135857_create_projects_table.php
+++ database/migrations/2020_06_15_135857_create_projects_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateVisitsTable extends Migration
+class CreateProjectsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,10 @@ class CreateVisitsTable extends Migration
      */
     public function up()
     {
-        Schema::create('visits', function (Blueprint $table) {
+        Schema::create('projects', function (Blueprint $table) {
             $table->increments('id');
-            $table->dateTime('date');
-            $table->integer('call_costumer_in')->nullable();
-            $table->boolean('hoa')->nullable();
-            $table->boolean('water_smart_rebate')->nullable();
-            $table->string('type')->nullable();
+            $table->integer('costumer_id');
+            $table->integer('visit_id');
             $table->timestamps();
         });
     }
@@ -31,6 +28,6 @@ class CreateVisitsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('visits');
+        Schema::dropIfExists('projects');
     }
 }
