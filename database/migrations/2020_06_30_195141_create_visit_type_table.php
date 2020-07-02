e61cc21428d0026b5dedaef2079494c2677e96af
Matheus Carvalho <matheus9bh@hotmail.com>
2020-07-01 22:45:13 -0300

    deploy#2

--- database/migrations/2020_06_30_195141_create_visit_type_table.php
+++ database/migrations/2020_06_30_195141_create_visit_type_table.php
@@ -14,7 +14,6 @@ class CreateVisitTypeTable extends Migration
     public function up()
     {
         Schema::create('visit_type', function (Blueprint $table) {
-            $table->increments('id');
             $table->integer('visit_id')->unsigned();
             $table->integer('type_id')->unsigned();
             $table->foreign('visit_id')->references('id')->on('visits')->onDelete('cascade');
