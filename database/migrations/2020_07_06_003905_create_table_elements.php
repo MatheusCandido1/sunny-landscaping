ba247b7a5cc1b151c397d163c63a9b606d81a04c
Matheus <matheus9bh@hotmail.com>
2020-07-05 21:41:16 -0300

    add-elements-migration

---
+++ database/migrations/2020_07_06_003905_create_table_elements.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableElements extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('elements', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('target');
+            $table->float('investment');
+            $table->float('quantity');
+            $table->string('description');
+            $table->float('unit_price');
+            $table->string('type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('elements');
+    }
+}
