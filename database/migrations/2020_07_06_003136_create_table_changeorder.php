adddba62784457d6adee5421889682a8113a5f82
Matheus <matheus9bh@hotmail.com>
2020-07-05 21:38:00 -0300

    add-change_orders_migration

---
+++ database/migrations/2020_07_06_003136_create_table_changeorder.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableChangeorder extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('change_orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->dateTime('date');
+            $table->float('original_contract_amount');
+            $table->float('change_order_amount');
+            $table->float('revise_contract_amout');
+            $table->boolean('status');
+            $table->integer('visit_id')->unsigned();
+            $table->foreign('visit_id')->references('id')->on('visits')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('change_orders');
+
+    }
+}
