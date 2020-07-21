cd98e86b808f688aec9ac4470f650004fece81f1
Matheus <matheus9bh@hotmail.com>
2020-07-21 16:34:26 -0300

    fix-front-page-pdf

--- resources/views/pdfs/front.blade.php
+++ resources/views/pdfs/front.blade.php
@@ -34,73 +34,69 @@
     </colgroup>
     <thead>
       <tr>
-        <th class="tg-k27y">PO:</th>
-        <th class="tg-k27y">{{$data[0]->project_name}} </th>
-        <th class="tg-k27y"></th>
+        <th class="tg-k27y"> <span style="text-align: center; font-size: 13px; font-weight: bold">PO:</span></th>
+        <th class="tg-k27y" colspan="2"><span style="text-align: center; font-size: 16px; font-weight: bold">{{$data[0]->project_name}}</span></th>
         <th class="tg-k27y">Job Name:</th>
-      <th class="tg-k27y"> </th>
+      <th class="tg-k27y" colspan="5"> <span style="text-align: center; font-size: 16px; font-weight: bold">{{$data[0]->address}}</span></th>
         <th class="tg-k27y">Seller:</th>
-        <th class="tg-u5dg" colspan="5">{{$data[0]->sel_name}}</th>
+        <th class="tg-k27y" ><span style="text-align: center; font-size: 13px; font-weight: bold">{{$data[0]->sel_name}}</span></th>
       </tr>
     </thead>
     <tbody>
       <tr>
-        <td class="tg-k27y">Name:</td>
-      <td class="tg-k27y" colspan="3">{{$data[0]->customer_name}}</td>
+        <td class="tg-k27y"><span style="text-align: center; font-size: 13px; font-weight: bold">Name:</span></td>
+      <td class="tg-k27y" colspan="3"> <span style="text-align: center; font-size: 16px; font-weight: bold">{{$data[0]->customer_name}}</span></td>
         <td class="tg-k27y">HOA</td>
-      <td class="tg-k27y"> {{ $data[0]->hoa == 0 ? '( ) Yes / (x) No' : '(x) Yes / ( ) No'}} </td>
+      <td class="tg-k27y" style="width: 10%"> {{ $data[0]->hoa == 0 ? '( ) Yes / (x) No' : '(x) Yes / ( ) No'}} </td>
         <td class="tg-k27y" colspan="5"></td>
       </tr>
       <tr>
-        <td class="tg-k27y">Address:</td>
-      <td class="tg-k27y" colspan="3">{{$data[0]->address}}</td>
+        <td class="tg-k27y"><span style="text-align: center; font-size: 13px; font-weight: bold">Address:</span></td>
+      <td class="tg-k27y" colspan="3"><span style="text-align: center; font-size: 16px; font-weight: bold">{{$data[0]->address}}</span></td>
         <td class="tg-k27y">E-mailed on:</td>
         <td class="tg-k27y">__/__/____</td>
         <td class="tg-k27y" colspan="5"></td>
       </tr>
        <tr>
-        <td class="tg-k27y">City:</td>
+        <td class="tg-k27y"><span style="text-align: center; font-size: 13px; font-weight: bold">City:</span></td>
         <td class="tg-k27y">{{$data[0]->city_name}}</td>
-        <td class="tg-u5dg">Zip Code:</td>
-        <td class="tg-k27y">{{$data[0]->zipcode}}</td>
+        <td class="tg-u5dg" style="text-align: center">Zip Code:</td>
+        <td class="tg-k27y"> <span style="text-align: center; font-size: 13px; font-weight: bold">{{$data[0]->zipcode}}</span></td>
         <td class="tg-k27y" colspan="7">Notes:</td>
       </tr>
       <tr>
-        <td class="tg-k27y">Gate Code:</td>
+        <td class="tg-k27y"><span style="text-align: center; font-size: 13px; font-weight: bold">Gate Code:</span></td>
         <td class="tg-k27y" colspan="3">{{$data[0]->gate_code}}</td>
         <td class="tg-k27y" colspan="7" rowspan="7"></td>
       </tr>
       <tr>
-        <td class="tg-k27y">Home #:</td>
-        <td class="tg-k27y" colspan="3"></td>
-      </tr>
-      <tr>
-        <td class="tg-k27y">E-mail:</td>
+        <td class="tg-k27y"><span style="text-align: center; font-size: 13px; font-weight: bold">E-mail:</span></td>
         <td class="tg-k27y" colspan="3">{{$data[0]->email}}</td>
       </tr>
       <tr>
-        <td class="tg-k27y">Cross Streets:</td>
+        <td class="tg-k27y"><span style="text-align: center; font-size: 13px; font-weight: bold">Cross Streets:</span></td>
         <td class="tg-k27y" colspan="3">{{$data[0]->cross_street1}} / {{$data[0]->cross_street2}}</td>
       </tr>
       <tr>
-        <td class="tg-k27y">Start Date:</td>
+        <td class="tg-k27y"><span style="text-align: center; font-size: 13px; font-weight: bold">Start Date:</span></td>
         <td class="tg-k27y" colspan="3"></td>
       </tr>
       <tr>
-        <td class="tg-k27y">Parcel #</td>
+        <td class="tg-k27y"><span style="text-align: center; font-size: 13px; font-weight: bold">Parcel #:</span></td>
         <td class="tg-k27y" colspan="3">{{$data[0]->parcel_number}}</td>
       </tr>
       <tr>
-        <td class="tg-k27y">Referral:</td>
+        <td class="tg-k27y"><span style="text-align: center; font-size: 13px; font-weight: bold">Referral:</span></td>
         <td class="tg-k27y" colspan="3">{{$data[0]->ref_name}}</td>
       </tr>
       <tr>
-        <td class="tg-k27y">Project Drawing:</td>
+        <td class="tg-k27y"><span style="text-align: center; font-size: 13px; font-weight: bold">Project Drawing:</span></td>
         <td class="tg-k27y">2D / 3D</td>
-        <td class="tg-k27y" colspan="9"></td>
+        <td class="tg-k27y"></td>
+        <td class="tg-k27y"></td>
       </tr>
       <tr>
-        <td class="tg-k27y">Sold:</td>
+        <td class="tg-k27y"><span style="text-align: center; font-size: 13px; font-weight: bold">Sold:</span></td>
         <td class="tg-k27y"></td>
         <td class="tg-k27y"></td>
         <td class="tg-k27y">Date</td>
@@ -113,46 +109,46 @@
         <td class="tg-rykj"></td>
       </tr>
       <tr>
-        <td class="tg-k27y">HOA Paperwork:</td>
+        <td class="tg-k27y" style="width: 20%"><span style="text-align: center; font-size: 13px; font-weight: bold">HOA Paperwork:</span></td>
         <td class="tg-k27y"></td>
-        <td class="tg-k27y">Security Deposit:</td>
+        <td class="tg-k27y" style="width: 15%">Security Deposit:</td>
         <td class="tg-k27y"></td>
         <td class="tg-k27y"></td>
         <td class="tg-k27y">Check</td>
-        <td class="tg-w2dt">#__________</td>
+        <td class="tg-w2dt">#________</td>
         <td class="tg-k27y"><span style="font-weight:bold">CC</span></td>
         <td class="tg-k27y"></td>
         <td class="tg-k27y">$</td>
         <td class="tg-rykj"></td>
       </tr>
       <tr>
-        <td class="tg-k27y">Material List:</td>
+        <td class="tg-k27y"><span style="text-align: center; font-size: 13px; font-weight: bold">Material List:</span></td>
         <td class="tg-k27y"></td>
         <td class="tg-k27y">Downpayment:</td>
         <td class="tg-k27y"></td>
         <td class="tg-k27y"></td>
         <td class="tg-k27y">Check</td>
-        <td class="tg-w2dt">#__________</td>
+        <td class="tg-w2dt">#________</td>
         <td class="tg-k27y"><span style="font-weight:bold">CC</span></td>
         <td class="tg-k27y"></td>
         <td class="tg-k27y">$</td>
         <td class="tg-rykj"></td>
       </tr>
       <tr>
-        <td class="tg-k27y">Contractor's Board PWW</td>
+        <td class="tg-k27y"><span style="text-align: center; font-size: 13px; font-weight: bold">Contractor's Board PWW:</span></td>
         <td class="tg-k27y"></td>
         <td class="tg-k27y">Balance:</td>
         <td class="tg-k27y"></td>
         <td class="tg-k27y"></td>
         <td class="tg-k27y">Check</td>
-        <td class="tg-w2dt">#__________</td>
+        <td class="tg-w2dt">#________</td>
         <td class="tg-k27y"><span style="font-weight:bold">CC</span></td>
         <td class="tg-k27y"></td>
         <td class="tg-k27y">$</td>
         <td class="tg-rykj"></td>
       </tr>
       <tr>
-        <td class="tg-k27y">Unconditional Waiver Release</td>
+        <td class="tg-k27y"><span style="text-align: center; font-size: 13px; font-weight: bold">Unconditional Waiver Release:</span></td>
         <td class="tg-k27y"></td>
         <td class="tg-u5dg" colspan="9">Notes:</td>
       </tr>
