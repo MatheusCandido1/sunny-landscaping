039eaf9223a2907f23479421a4e521d18d45f85f
Matheus Cândido <matheus9bh@hotmail.com>
2020-09-29 19:26:27 -0300

    edit-changeorder

--- resources/views/pdfs/change.blade.php
+++ resources/views/pdfs/change.blade.php
@@ -59,8 +59,10 @@ footer {
                 height: 50px; 
                 text-align: center;
                 color: #777;
+                border-top: 1px solid #f5a15c;
             }
 
+            .page_break { page-break-before: always; }
 #image img {
 /* the actual 'watermark' */
 position: absolute;
@@ -82,6 +84,12 @@ opacity: 0.2; /* Firefox, Chrome, Safari, Opera, IE >= 9 (preview) */
 
 <p class=MsoNormal align=right style='margin-bottom:12.0pt;text-align:right;
 border:none'>
+<footer class=MsoNormal>
+  <span lang=EN-US style='font-size:8.0pt'>We
+  accept cash, check and credit card. For payments with credit card, will be
+  added a 3% processing fee. Prices are valid for 60 days after the date of
+  proposal, and are subject to change after that period due to raw materials or
+  labor costs.</span></footer>
 
 <table class=2 border=0 cellspacing=0 cellpadding=0 width=400 style='border-collapse:
  collapse'>
@@ -151,13 +159,19 @@ border:none'>
 <p><span style="font-size: 12;">ORIGINAL CONTRACT AMOUNT: US$ {{number_format($data->original_contract_amount,2)}} </span></p>
 <p><span style="font-size: 12;">CHANGE ORDER AMOUNT #{{$data->change_order_key}}: US$ {{number_format($data->change_order_amount,2)}} &nbsp; </span></p>
 <p><span style="font-size: 12;"><strong>REVISED CONTRACT AMOUNT: US$  {{number_format($data->revised_contract_amount,2)}} </strong><span></p>
-<span style="font-size: 12;">We hereby agree to the above as an </span><strong>{{$data->option_1}} </strong><span style="font-size: 12;">project costs to be </span><strong>{{$data->option_1 == "Additional" ? 'Added':'Removed'}} </strong><span style="font-size: 12;">{{$data->option_1 == "Additional" ? 'to':'from'}} the original scope of work. Payment for this Change Order is due upon completion of the change order work. All provisions of contracts between the parties are applicable to this agreement. This Change Order may extend the time required to complete the project.&nbsp;</span></p>
-<footer class=MsoNormal>
-<span lang=EN-US style='font-size:8.0pt'>We
-accept cash, check and credit card. For payments with credit card, will be
-added a 3% processing fee. Prices are valid for 60 days after the date of
-proposal, and are subject to change after that period due to raw materials or
-labor costs.</span></footer>
+
+  <div class="page_break"></div>
+  
+  <div id="image">
+    <img src="https://i.ibb.co/n6SVRQr/Logo-Sun.jpg" alt="..." />
+  </div>
+  <span style="font-size: 12;">We hereby agree to the above as an </span>
+  <strong>{{$data->option_1}} </strong>
+  <span style="font-size: 12;">project costs to be </span>
+  <strong>{{$data->option_1 == "Additional" ? 'Added':'Removed'}} </strong>
+  <span style="font-size: 12;">{{$data->option_1 == "Additional" ? 'to':'from'}} the original scope of work. Payment for this Change Order is due upon completion of the change order work. All provisions of contracts between the parties are applicable to this agreement. This Change Order may extend the time required to complete the project.&nbsp;
+  </span></p>
+
 </div>
 <br>
 <br>
@@ -167,7 +181,7 @@ labor costs.</span></footer>
 <br>
 <br>
 <div style="text-align: center">
-  <img  style="height: 35%; width: 35%; position: absolute; margin-top: -60px; margin-left: -60px" src="https://imagensbrasil.org/images/2020/08/10/Imagem1.png" data-holder-rendered="true" />       
+  <img  style="height: 35%; width: 35%; position: absolute; margin-top: -60px; margin-left: -60px" src="https://i.ibb.co/4jY1dHg/signature.png" data-holder-rendered="true" />       
 </div>
 <p class=MsoNormal style='margin-bottom:0;margin-bottom:.0001pt;line-height:
 normal;border:none'><span style='font-size:16.0pt'>Glaucia Alves</span> </p>
@@ -195,6 +209,13 @@ normal;border:none'>Sunny Landscaping &amp; Pavers Design LLC</p>
   </td>
  </tr>
 </table>
+
+<footer class=MsoNormal>
+  <span lang=EN-US style='font-size:8.0pt'>We
+  accept cash, check and credit card. For payments with credit card, will be
+  added a 3% processing fee. Prices are valid for 60 days after the date of
+  proposal, and are subject to change after that period due to raw materials or
+  labor costs.</span></footer>
 </body>
 
 </html>
